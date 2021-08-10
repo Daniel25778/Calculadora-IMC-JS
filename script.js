a41fd3889a657e1f7fa3812521388eefa132c562
DANIEL WILSON ALVES DOS SANTOS <21192960@CFP127ALUNO.LOCAL>
2021-08-10 11:29:53 -0300

    calculo imc

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-const nome = document.getElementById("nome");
+const nome = document.getElementById("nome")
+const altura = document.getElementById("altura")
+const peso = document.getElementById("peso")
+const calcular = document.getElementById("calcular")
+const resultado = document.getElementById("resultado")
+
+function calculo() {
+    if (nome.value !== "" && altura !== "" && peso !== "") {
+        resultado.textContent = parseFloat(peso.value) / (parseFloat(altura.value ** 2))
+
+    } else {
+        alert("Preencha todos os campos!!")
+    }
+}
+
+calcular.addEventListener("click", calculo)
